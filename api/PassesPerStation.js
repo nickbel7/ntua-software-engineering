212a216a7f6caffa9f1f996a1d39089aad3c20fd
el18134 <94307572+el18134@users.noreply.github.com>
2021-12-21 23:56:01 +0200

    Add files via upload

--- api/PassesPerStation.js
+++ api/PassesPerStation.js
@@ -6,21 +6,23 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 client.connect();
-router.get('/:station_id/:date_from/:date_to', (req,res) => {
+router.get('/:station_id/:date_from/:date_to', async (req,res) => {
 	const { station_id,date_from,date_to } = req.params;
-	client.query("Select * from passes where station_id=$1 and pass_timestamp between $2 and $3",[station_id,date_from,date_to],(err, result)=>{
-		console.log(typeof req.params.stationID);
-        if(!err){
-           // res.send(result.rows);
-            const str =`Station: ${req.params.station_id}`;
-            res.send(str);
-        }
-        else{
-        	res.send("problem!")
-        }
-       
-   });
+	try {
+	const result = await client.query("Select passes.pass_id, passes.pass_timestamp, tags.vehicle_id, tags.provider_id, passes.pass_rate FROM tags INNER JOIN providers USING (provider_id) INNER JOIN stations USING (provider_id) INNER JOIN passes USING (station_id) where station_id=$1 and pass_timestamp between $2 and $3",[station_id,date_from,date_to]);
+	console.log(station_id, date_from, date_to);
+	const response = {
+		Station:station_id,
+		PeriodFrom:date_from,
+		PeriodTo:date_to,
+		PassesList:result.rows,
+	}
+	res.json(response);
+	}
+	catch (err) {
+		console.log(err);
+	}
+ 
  	client.end;
-})
+});
 module.exports = router;
-
