7024a43a11aa16917501337233e9700dd820b4c2
christidia <94255085+christidia@users.noreply.github.com>
2021-12-21 23:27:01 +0200

    Add files via upload

--- api/PassesAnalysis.js
+++ api/PassesAnalysis.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const router = express.Router();
+const client2 = require('./connection.js');
 
 
+client2.connect();
 router.get('/:op1_ID/:op2_ID/:date_from/:date_to', (req,res) => {
-	res.end('op1 ID is '+ req.params.op1_ID);
-});
+	const { op1_ID,op2_ID,date_from,date_to }= req.params;
+	client2.query("Select * from passes where tag_id LIKE $1 AND station_id LIKE $2and pass_timestamp between $3 and $4",[op1_ID+'%',op2_ID+'%',date_from,date_to],(err, result)=>{
+        if(!err){
+           res.send(result.rows);
+            
+        }
+        else{
+        	res.send("problem!")
+        }
+       
+   });
+ 	client2.end;
+})
 module.exports = router;
--- api/connection.js
+++ api/connection.js
@@ -1,6 +1,6 @@
-const {Client} = require('pg')
+const Pool = require('pg').Pool;
 
-const client = new Client({
+const pool = new Pool({
     host: "localhost",
     user: "postgres",
     port: 5432,
@@ -8,4 +8,4 @@ const client = new Client({
     database: "diode"
 })
 
-module.exports = client;
+module.exports = pool;
