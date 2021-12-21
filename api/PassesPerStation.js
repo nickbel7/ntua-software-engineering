ff3ba1f9e999b774f67428b0ed9bbb0387f242af
christidia <94255085+christidia@users.noreply.github.com>
2021-12-21 21:33:55 +0200

    Add files via upload

--- api/PassesPerStation.js
+++ api/PassesPerStation.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router();
+const client = require('./connection.js');
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-
-router.get('/:stationID/:date_from/:date_to', (req,res) => {
-	res.end('Station ID is '+ req.params.stationID);
-});
+client.connect();
+router.get('/:station_id/:date_from/:date_to', (req,res) => {
+	const { station_id,date_from,date_to } = req.params;
+	client.query("Select * from passes where station_id=$1 and pass_timestamp between $2 and $3",[station_id,date_from,date_to],(err, result)=>{
+		console.log(typeof req.params.stationID);
+        if(!err){
+            res.send(result.rows);
+        }
+        else{
+        	res.send("problem!")
+        }
+       
+   });
+ 	client.end;
+})
 module.exports = router;
+
