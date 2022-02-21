698966d083c2d0561bd84bb93403819283178fab
Yiannos <paranomosyiannos@gmail.com>
2022-02-21 20:37:26 +0200

    Message

---
+++ cli/commands/PassesAnalysis.js
@@ -0,0 +1,9 @@
+const axios = require ('axios');
+
+module.exports = function pccall(op1,op2,datefrom,dateto,format) {
+
+    let url='https://localhost:9103/interoperability/api/PassesAnalysis/'+op1+'/'+op2+'/'+datefrom+'/'+dateto+'?format='+format;
+    axios.get(url).then( resp => {
+        console.log(resp.data);
+    });
+}
