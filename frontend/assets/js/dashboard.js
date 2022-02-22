461845d933ad7a13120ea671666b0a62e7e270b9
ElinaSyr <elinasyrr@gmail.com>
2022-02-22 21:48:05 +0200

    Frontend + cli

--- frontend/assets/js/dashboard.js
+++ frontend/assets/js/dashboard.js
@@ -2,17 +2,18 @@ const ctx = document.getElementById('bar-graph').getContext('2d');
 const bar_graph = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['02/03', '03/03', '04/03', '05/03', '06/03', '07/03'],
+        labels: ['Aodos', 'Gefyra', 'Egnatia', 'Kentriki Odos', 'Moreas', 'Nea Odos', 'Olympia Odos'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19, 3, 5, 2, 3,20],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -20,7 +21,8 @@ const bar_graph = new Chart(ctx, {
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)'
             ],
             borderWidth: 1
         }]
