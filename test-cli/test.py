538258cc9fd2889044b96eaed4aadc72b3a07677
Tom Spyropoulos <tomody.spy@gmail.com>
2022-02-24 11:45:28 +0200

    added passes cost testing

--- test-cli/test.py
+++ test-cli/test.py
@@ -1,4 +1,3 @@
-from cgi import test
 import pandas as pd 
 from pathlib import Path
 from colorama import Fore, Style
@@ -8,7 +7,7 @@ providers = {"AO": "1", "GF": "2", "EG": "3", "KO": "4", "MR": "5", "NE": "6" , 
 func = {"passesperstation": 1 , "passesanalysis" : 2, "passescost": 3, "chargesby" : 4,
          "healthcheck":5, "resetpasses": 6, "resetstations":7,"resetvehicles":8}
 
-def pa_pc_param(line):
+def pa_param(line):
     ret=[]
     check=True
     for i in range (len(line[0])):
@@ -22,12 +21,38 @@ def pa_pc_param(line):
     ret.append((line[1]))
     return ret
 
+def pc_param(line):
+    ret=[]
+    prev=""
+    same = False
+    check=True
+    for i in range (len(line[0])):
+        if line[0][i]=="-": 
+            continue
+        if check:
+            if prev == str(line[0][i:(i+2)]):
+                same = True
+            prev = str(line[0][i:(i+2)])
+            ret.append(prev)
+            check=False
+        else:
+            check=True
+    if not same:
+        if str(line[1]) == 'nan':
+            ret.append('null')
+        else:
+            ret.append((line[1]))
+        
+    else:
+        ret.append('null')
+    return ret
+
 def pps_param(line):
     
     return [line[0],line[1]]
 
 def takedate(line):
-    d=pa_pc_param(line)
+    d=pa_param(line)
     start_date=d[0]+d[1]+d[2]+"01"
     month = "{:02d}".format(int(d[2])+1)
     end_date=d[0]+d[1]+month+"01"
@@ -40,7 +65,7 @@ def makecli(date, param, testfunc):
     elif(testfunc == func["passesanalysis"]):
         ret=f"cd ../cli && node --no-warnings index.js passesanalysis --op1 {providers[param[1]]} --op2 {providers[param[0]]} --datefrom {date[0]} --dateto {date[1]} --format json && cd../test-cli"
     elif(testfunc == func["passescost"]):
-        ret=f"cd ../cli && node --no-warnings index.js passescost --op1 {providers[param[0]]} --op2 {providers[param[1]]} --datefrom {date[0]} --dateto {date[1]} --format json && cd../test-cli"
+        ret=f"cd ../cli && node --no-warnings index.js passescost --op1 {providers[param[1]]} --op2 {providers[param[0]]} --datefrom {date[0]} --dateto {date[1]} --format json && cd../test-cli"
     elif(testfunc == func["chargesby"]):
         pass
     elif(testfunc==func["healthcheck"]):
@@ -67,6 +92,9 @@ def programout(command,testfunc):
         ans,_,_=txt[2].partition("}")
         if ans[0]!='n':
             ans = ans[1:-2]
+            ans = float(ans)
+        else:
+            ans = 'null'
     elif(testfunc == func["chargesby"]):
         pass
     elif(testfunc== func["healthcheck"]):
@@ -79,7 +107,7 @@ def programout(command,testfunc):
         ans = str(ans)
     return ans
 
-file=Path("sampledata01_christina_yiannos.xlsx")
+file=Path("sampledata01_christina_yiannos.xlsm")
 
 #Passes Per Station Testing
 print(Fore.YELLOW+"PASSES PER STATION TEST START"+Style.RESET_ALL)
@@ -88,7 +116,7 @@ passesperstation = pd.read_excel(file, sheet_name="number_of_passes_per_station"
 line = passesperstation.values.tolist()
 line = line[2:]
 
-test_num=10
+test_num=100
 passed = 0
 for i in range(0,test_num):
     if(line[i][0][0]=="2"):
@@ -122,14 +150,14 @@ passesanalysis=pd.read_excel(file, sheet_name="passes_analysis")
 line=passesanalysis.values.tolist()
 line= line[2:]
 
-test_num=10
+test_num=100
 passed = 0
 for i in range(0,test_num):
     if(line[i][0][0]=="2"):
         date=takedate(line[i])
         passed = passed + 1
         continue
-    param=pa_pc_param(line[i])
+    param=pa_param(line[i])
     command=makecli(date,param,func["passesanalysis"])
     value = str(programout(command,func["passesanalysis"]))
     expected = str(param[2])
@@ -155,22 +183,28 @@ print(Fore.YELLOW+"PASSES COST TEST START"+Style.RESET_ALL)
 passescost=pd.read_excel(file, sheet_name="passes_cost")
 line=passescost.values.tolist()
 
-test_num=10
+test_num=100
 passed = 0
 for i in range(0,test_num):
     if(line[i][0][0]=="2"):
         date=takedate(line[i])
         passed = passed + 1
         continue
-    param=pa_pc_param(line[i])
+    param=pc_param(line[i])
     command=makecli(date,param,func["passescost"])
-    value = str(programout(command,func["passescost"]))
-    expected = str(param[2])
-    if value==expected:
-        passed = passed + 1
+    value = programout(command,func["passescost"])
+    expected = param[2]   
+
+    if type(value) != type(expected):
+        print(Fore.RED + f"failed! value: {value}, expected:{expected}!")
     else:
-        pass
-        #print(Fore.RED + "failed" + " " + value)
+        if expected == 'null' or expected == 'nan':
+            passed = passed + 1
+        elif abs(value-expected)<0.001:
+            passed = passed + 1
+        else:
+            print(Fore.RED + f"failed! value: {value}, expected:{expected}!")
+
     
 print(f"Passes: {passed}/{test_num}")
 
